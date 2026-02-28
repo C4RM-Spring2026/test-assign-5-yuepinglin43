112fe434c3551f1e588db3078c5da99ef3a0a8fd
yuepinglin43 <484641582@qq.com>
2026-02-27 19:39:59 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,6 +1,6 @@
 import numpy as np
 def FizzBuzz(start, finish):
-    numvec = np.arange(start,finish)
+    numvec = np.arange(start,finish + 1)
     objvec = np.array(numvec,dtype = object)
     objvec[numvec % 3 == 0] = "fizz"
     objvec[numvec % 5 == 0] = "buzz"
